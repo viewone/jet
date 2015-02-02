70bc1a5f035a4de296da1228e0a69ac2023ff81d
Piotr Kierzniewski <kierzniak@gmail.com>
2015-02-02 22:45:45 +0100

    Changes

--- shell/initial-setup.sh
+++ shell/initial-setup.sh
@@ -1,41 +1,24 @@
 #!/bin/bash
 
-VAGRANT_CORE_FOLDER="/vagrant"
+CORE_DIRECTORY="/home/viewone/jet"
 
-OS=$(/bin/bash "${VAGRANT_CORE_FOLDER}/viewone/shell/os-detect.sh" ID)
-CODENAME=$(/bin/bash "${VAGRANT_CORE_FOLDER}/viewone/shell/os-detect.sh" CODENAME)
+OS=$(/bin/bash "${CORE_DIRECTORY}/shell/os-detect.sh" ID)
+CODENAME=$(/bin/bash "${CORE_DIRECTORY}/shell/os-detect.sh" CODENAME)
 
-if [[ ! -d /.viewone-stuff ]]; then
-    mkdir /.viewone-stuff
+if [[ ! -d "${CORE_DIRECTORY}" ]]; then
+    mkdir "${CORE_DIRECTORY}"
 
-    echo "${VAGRANT_CORE_FOLDER}" > "/.viewone-stuff/vagrant-core-folder.txt"
+    echo "${CORE_DIRECTORY}" > "/${CORE_DIRECTORY}/core-directory.txt"
 
-    cat "${VAGRANT_CORE_FOLDER}/viewone/shell/self-promotion.txt"
-    echo "Created directory /.viewone-stuff"
+    cat "${CORE_DIRECTORY}/shell/self-promotion.txt"
+    echo "Created directory ${CORE_DIRECTORY}"
 fi
 
-if [[ ! -f /.viewone-stuff/initial-setup-repo-update ]]; then
+if [[ ! -f "${CORE_DIRECTORY}/initial-setup-repo-update" ]]; then
     if [ "${OS}" == 'debian' ] || [ "${OS}" == 'ubuntu' ]; then
         echo "Running initial-setup apt-get update"
         apt-get update >/dev/null
-        touch /.viewone-stuff/initial-setup-repo-update
+        touch "${CORE_DIRECTORY}/initial-setup-repo-update"
         echo "Finished running initial-setup apt-get update"
-    elif [[ "${OS}" == 'centos' ]]; then
-        echo "Running initial-setup yum update"
-        yum update -y >/dev/null
-        echo "Finished running initial-setup yum update"
-
-        echo "Installing basic development tools (CentOS)"
-        yum -y groupinstall "Development Tools" >/dev/null
-        echo "Finished installing basic development tools (CentOS)"
-        touch /.viewone-stuff/initial-setup-repo-update
     fi
-fi
-
-if [[ "${OS}" == 'ubuntu' && ("${CODENAME}" == 'lucid' || "${CODENAME}" == 'precise') && ! -f /.viewone-stuff/ubuntu-required-libraries ]]; then
-    echo 'Installing basic curl packages (Ubuntu only)'
-    apt-get install -y libcurl3 libcurl4-gnutls-dev >/dev/null
-    echo 'Finished installing basic curl packages (Ubuntu only)'
-
-    touch /.viewone-stuff/ubuntu-required-libraries
 fi
--- shell/librarian-puppet-vagrant.sh
+++ shell/librarian-puppet-vagrant.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-VAGRANT_CORE_FOLDER=$(cat "/.viewone-stuff/vagrant-core-folder.txt")
+CORE_DIRECTORY=$(cat "/home/viewone/jet/core-directory.txt")
 
-OS=$(/bin/bash "${VAGRANT_CORE_FOLDER}/viewone/shell/os-detect.sh" ID)
-CODENAME=$(/bin/bash "${VAGRANT_CORE_FOLDER}/viewone/shell/os-detect.sh" CODENAME)
+OS=$(/bin/bash "${CORE_DIRECTORY}/shell/os-detect.sh" ID)
+CODENAME=$(/bin/bash "${CORE_DIRECTORY}/shell/os-detect.sh" CODENAME)
 
 # Directory in which librarian-puppet should manage its modules directory
 PUPPET_DIR=/etc/puppet/
@@ -11,20 +11,11 @@ PUPPET_DIR=/etc/puppet/
 $(which git > /dev/null 2>&1)
 FOUND_GIT=$?
 
-if [ "${FOUND_GIT}" -ne '0' ] && [ ! -f /.viewone-stuff/librarian-puppet-installed ]; then
-    $(which apt-get > /dev/null 2>&1)
-    FOUND_APT=$?
-    $(which yum > /dev/null 2>&1)
-    FOUND_YUM=$?
+if [ "${FOUND_GIT}" -ne '0' ] && [ ! -f "${CORE_DIRECTORY}/librarian-puppet-installed" ]; then
 
     echo 'Installing git'
 
-    if [ "${FOUND_YUM}" -eq '0' ]; then
-        yum -q -y makecache
-        yum -q -y install git
-    else
-        apt-get -q -y install git-core >/dev/null
-    fi
+    apt-get -q -y install git-core > /dev/null
 
     echo 'Finished installing git'
 fi
@@ -34,40 +25,20 @@ if [[ ! -d "${PUPPET_DIR}" ]]; then
     echo "Created directory ${PUPPET_DIR}"
 fi
 
-cp "${VAGRANT_CORE_FOLDER}/viewone/puppet/Puppetfile" "${PUPPET_DIR}"
+cp "${CORE_DIRECTORY}/puppet/Puppetfile" "${PUPPET_DIR}"
 echo "Copied Puppetfile"
 
 if [ "${OS}" == 'debian' ] || [ "${OS}" == 'ubuntu' ]; then
-    if [[ ! -f /.viewone-stuff/librarian-base-packages ]]; then
+    if [[ ! -f "${CORE_DIRECTORY}/librarian-base-packages" ]]; then
         echo 'Installing base packages for librarian'
         apt-get install -y build-essential ruby-dev >/dev/null
         echo 'Finished installing base packages for librarian'
 
-        touch /.viewone-stuff/librarian-base-packages
+        touch "${CORE_DIRECTORY}/librarian-base-packages"
     fi
 fi
 
-if [ "${OS}" == 'ubuntu' ]; then
-    if [[ ! -f /.viewone-stuff/librarian-libgemplugin-ruby ]]; then
-        echo 'Updating libgemplugin-ruby (Ubuntu only)'
-        apt-get install -y libgemplugin-ruby >/dev/null
-        echo 'Finished updating libgemplugin-ruby (Ubuntu only)'
-
-        touch /.viewone-stuff/librarian-libgemplugin-ruby
-    fi
-
-    if [ "${CODENAME}" == 'lucid' ] && [ ! -f /.viewone-stuff/librarian-rubygems-update ]; then
-        echo 'Updating rubygems (Ubuntu Lucid only)'
-        echo 'Ignore all "conflicting chdir" errors!'
-        gem install rubygems-update >/dev/null
-        /var/lib/gems/1.8/bin/update_rubygems >/dev/null
-        echo 'Finished updating rubygems (Ubuntu Lucid only)'
-
-        touch /.viewone-stuff/librarian-rubygems-update
-    fi
-fi
-
-if [[ ! -f /.viewone-stuff/librarian-puppet-installed ]]; then
+if [[ ! -f "${CORE_DIRECTORY}/librarian-puppet-installed" ]]; then
     echo 'Installing librarian-puppet'
     gem install librarian-puppet >/dev/null
     echo 'Finished installing librarian-puppet'
@@ -76,16 +47,9 @@ if [[ ! -f /.viewone-stuff/librarian-puppet-installed ]]; then
     cd "${PUPPET_DIR}" && librarian-puppet install --clean >/dev/null
     echo 'Finished running initial librarian-puppet'
 
-    touch /.viewone-stuff/librarian-puppet-installed
+    touch "${CORE_DIRECTORY}/librarian-puppet-installed"
 else
     echo 'Running update librarian-puppet'
     cd "${PUPPET_DIR}" && librarian-puppet update >/dev/null
     echo 'Finished running update librarian-puppet'
-fi
-
-if [[ "${OS}" == 'centos' ]]; then
-    echo "Replacing puppetlabs-git module with custom"
-    rm -rf /etc/puppet/modules/git
-    git clone https://github.com/viewone/puppetlabs-git.git /etc/puppet/modules/git
-    echo "Finished replacing puppetlabs-git module with custom"
 fi
