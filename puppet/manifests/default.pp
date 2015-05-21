602403b588d6d9a2ca499cb56e956ae5f20b6fdb
Piotr Kierzniewski <kierzniak@gmail.com>
2015-05-21 12:16:25 +0200

    Remove trash and update puppet files

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -1,8 +1,8 @@
 class { 'server':
-    hostname => 'dev',
-    locale_default => 'pl_PL.UTF-8',
-    locale_available => ["pl_PL.UTF-8 UTF-8", "en_US.UTF-8 UTF-8", "de_DE.UTF-8 UTF-8"],
-    timezone => "Europe/Warsaw",
+    hostname => 'mci',
+    default_locale => 'en_US.UTF-8',
+    locales => ["en_US.UTF-8 UTF-8"],
+    timezone => "America/New_York",
     ntp_servers => [ '0.pl.pool.ntp.org', '1.pl.pool.ntp.org', '2.pl.pool.ntp.org', '3.pl.pool.ntp.org' ],
     packages => ['git-core', 'zsh']
 }
