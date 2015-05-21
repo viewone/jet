f841f71a072991fe2e36968cae6088b272a91510
Piotr Kierzniewski <kierzniak@gmail.com>
2015-05-21 21:28:53 +0200

    Update default.pp

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -1,8 +1,59 @@
 class { 'server':
-    hostname => 'mci',
     default_locale => 'en_US.UTF-8',
     locales => ["en_US.UTF-8 UTF-8"],
-    timezone => "America/New_York",
+    timezone => "Europe/Warsaw",
     ntp_servers => [ '0.pl.pool.ntp.org', '1.pl.pool.ntp.org', '2.pl.pool.ntp.org', '3.pl.pool.ntp.org' ],
-    packages => ['git-core', 'zsh']
+    hostname => 'mci.viewone.pl',
+    hosts => {
+      'localhost' => {
+        ip => '127.0.0.1',
+        host_aliases => 'localdomain.localhost localhost',
+      },
+      'localhost_debian' => {
+        ip => '127.0.0.1',
+        host_aliases => 'debian-wheezy-amd64.vagrantup.com debian-wheezy-amd64',
+      },
+      'localhost_mci' => {
+        ip => '127.0.0.1',
+        host_aliases => 'mci.vieowne.pl mci',
+      },
+      'localhost_ipv6' => {
+        ip => '::1',
+        host_aliases => 'localhost ip6-localhost ip6-loopback',
+      },
+      'localhost_ipv6-localnet' => {
+        ip => 'ff00::0',
+        host_aliases => 'ip6-localnet',
+      },
+      'localhost_ipv6-mcastprefix' => {
+        ip => 'ff00::0',
+        host_aliases => 'ip6-mcastprefix',
+      },
+      'localhost_ipv6-allnodes' => {
+        ip => 'ff00::1',
+        host_aliases => 'ip6-allnodes',
+      },
+      'localhost_ipv6-allrouters' => {
+        ip => 'ff00::2',
+        host_aliases => 'ip6-allrouters',
+      },
+      'remote' => {
+        ip => '10.0.2.15',
+        host_aliases => 'debian-wheezy-amd64.vagrantup.com debian-wheezy-amd64',
+      },
+      'remote_mci' => {
+        ip => '10.0.2.15',
+        host_aliases => 'mci.vieowne.pl mci',
+      },
+    },
+    users => {
+         'kierzniak' => {
+              uid => 500,
+              gid => 500,
+              key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDS9jdn3+3yRiGuwLn/lM3N7mq+bLWplrWoRDuHIRV4ah+PllaufOKNmUGiNHtL3EMtpj1gOaw/F3edv/A56h52xDDqiUxqXE/M9GLzKrp+FzuvWOdLqf8YHkZ33UFTgsFf9QUZBGw/0loCge5fd2YhmVSg1wPQtS06Sf9v1XulLbnMAiDX07UoAsCMTu29Qh7M4LMsQWUDIuu8OHY7sZn89EQm19B30yuxoi5hEtOz8rtBSJ+Phrf3dk9Qhe46fXOn4dgNK+PfbvJ5VkSxFLBI15QVhzqFVCTcISHXkF32pbjrFXNP7sHdNd7tjvPYH/dZZbtM3i5AtJfoNsUMmpCn',
+              key_name => 'kierzniak',
+              password => '$1$6ARpdYY0$JBsBOlNAjMoMjMuE17b2t.',
+         },
+    },
+    packages => ['git-core', 'zsh'],
 }
