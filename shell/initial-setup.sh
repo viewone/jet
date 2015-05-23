d8babddda5df425f76d04e2f49c3fec6f08e5da0
Piotr Kierzniewski <kierzniak@gmail.com>
2015-05-23 10:50:28 +0200

    We do not have to create jet directory because it was created in the installation process

--- shell/initial-setup.sh
+++ shell/initial-setup.sh
@@ -6,17 +6,12 @@ JET_FILES_DIRECTORY="/usr/local/jet"
 OS=$(/bin/bash "${JET_DIRECTORY}/shell/os-detect.sh" ID)
 CODENAME=$(/bin/bash "${JET_DIRECTORY}/shell/os-detect.sh" CODENAME)
 
-if [[ ! -d "${JET_FILES_DIRECTORY}" ]]; then
-    mkdir "${JET_FILES_DIRECTORY}"
-    echo "Created directory ${JET_FILES_DIRECTORY}"
-fi
-
 echo "${JET_DIRECTORY}" > "${JET_FILES_DIRECTORY}/core-directory.txt"
 
 if [[ ! -f "${JET_FILES_DIRECTORY}/initial-setup-repo-update" ]]; then
     if [ "${OS}" == 'debian' ] || [ "${OS}" == 'ubuntu' ]; then
         echo "Running initial-setup apt-get update"
-        apt-get update >/dev/null
+        apt-get update > /dev/null
         touch "${JET_FILES_DIRECTORY}/initial-setup-repo-update"
         echo "Finished running initial-setup apt-get update"
     fi
